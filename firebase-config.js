1f9fe8e6f80ff30f92f957f18b3180708dad0a5b
Minh Triet <minhtriet20012001@gmail.com>
2019-12-29 15:54:09 +0700

    safehouse

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDuoi3qeNF-UpQ-ah-fANJ4Kjq6qLUru2k",
+  authDomain: "controldevices-7e5ac.firebaseapp.com",
+  databaseURL: "https://controldevices-7e5ac.firebaseio.com",
+  projectId: "controldevices-7e5ac",
+  storageBucket: "controldevices-7e5ac.appspot.com",
+  messagingSenderId: "437527271123",
+  appId: "1:437527271123:web:abab33d4b274097e5c2626",
+  measurementId: "G-MX38NR918S"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
